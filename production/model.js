82070512be6d938caeb223e0469d0553e9733bb7
amj <169126481+amjoftllb@users.noreply.github.com>
2024-06-11 12:08:13 +0530

    Update model.js

--- production/model.js
+++ production/model.js
@@ -1,42 +1,50 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const validator = require("validator");
+const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchema = new Schema(
+const userSchema = new mongoose.Schema(
   {
-    avatar: {
+    fullName: {
       type: String,
-      //required: true,
+      required: [true, 'Please enter your name'],
+      maxLength: [30, 'Name cannot exceed 30 charaters'],
+      minLength: [4, 'Name should have more then 4 charaters'],
     },
-    firstName: {
+    email: {
       type: String,
-      required: [true, "Please enter your name"],
-      maxLength: [20, "lastName cannot exceed 20 charaters"],
-      minLength: [4, "firstName should have more then 4 charaters"],
+      required: [true, 'Please enter an email'],
+      unique: true,
+      validate: [validator.isEmail, 'Please enter a valid email'],
     },
-    lastName: {
+    password: {
       type: String,
-      required: [true, "Please enter your name"],
-      maxLength: [20, "lastName cannot exceed 20 charaters"],
-      minLength: [4, "lastName should have more then 4 charaters"],
+      required: [true, 'Please enter your password'],
+      minLength: [8, 'Password should be greater then 8 charaters'],
+    },
+    image: {
+      type: String,
+    },
+    zipcode: {
+      type: Number,
     },
     gender: {
       type: String,
-      required: [true, "Please select a gender"],
-      enum: ["Male", "Female", "Others"],
+      required: [true, 'Please select a gender'],
+      enum: ['Male', 'Female', 'Others'],
     },
-    birthDate: {
+    country: {
       type: String,
-      required: [true, "Please enter a date of birth"],
+      required: [true, 'Please select a country'],
+    },
+    dateOfBirth: {
+      type: String,
+      required: [true, 'Please enter a date of birth'],
       validate: {
         validator: function (value) {
           const dateRegex = /^\d{4}-(0[1-9]|1[0-2])-([12][0-9]|0[1-9]|3[01])$/;
           if (!dateRegex.test(value)) {
             return false;
           }
-          const parts = value.split("-");
+          const parts = value.split('-');
           const year = parseInt(parts[0], 10);
           const month = parseInt(parts[1], 10);
           const day = parseInt(parts[2], 10);
@@ -47,88 +55,85 @@ const userSchema = new Schema(
             date.getFullYear() === year
           );
         },
-        message: "Please enter a valid date of birth (YYYY-MM-DD)",
+        message: 'Please enter a valid date of birth (YYYY-MM-DD)',
       },
     },
     phoneNumber: {
       type: Number,
-      required: [true, "Please enter a Mobile Number"],
+      required: [true, 'Please enter a Mobile Number'],
     },
-    department: {
+    profession: {
       type: String,
-      required: [true, "Please select a department"],
+      required: [true, 'Please select a profession'],
       enum: [
-        "Computer IT",
-        "Mechanical",
-        "Cevil",
-        "Chemical",
-        "Other",
+        'Nutritionist',
+        'Dietitian',
+        'Nutritional therapist',
+        'Health Coach',
+        'Student',
+        'Other',
       ],
     },
-    email: {
+    professionCardNumber: {
       type: Number,
-      required: [true, "Please enter a Mobile Number"],
     },
-    password: {
+    nutrium: {
       type: String,
-      required: [true, "Please enter your password"],
-      minLength: [8, "Password should be greater then 8 charaters"],
+      required: [true, 'Please select What are you looking for in Nutrium?'],
     },
-    isAdmin: {
-      type: Boolean,
-      default: true,
-    },
-    refreshToken: {
+    workplace: {
       type: String,
+      required: [true, 'Please enter a Your Work Place'],
+    },
+    expertise: {
+      type: Array,
+      required: function () {
+        return this.profession !== 'Student';
+      },
+    },
+    clientPerMonth: {
+      type: String,
+      required: function () {
+        return this.profession !== 'Student';
+      },
+    },
+    courseEndDate: {
+      type: String,
+    },
+    resetToken: {
+      type: String,
+      default: null,
+    },
+    resetTokenExpires: {
+      type: Date,
+      default: null,
+    },
+    presentation: {
+      type: String,
+    },
+    aboutMe: {
+      type: String,
+    },
+    url: [
+      {
+        platform: {
+          type: String,
+        },
+        link: {
+          type: String,
+        },
+      },
+    ],
+    privacyStatus: {
+      type: String,
+      default: 'Private',
+    },
+    isActive: {
+      type: Number,
+      default: 1,
     },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-// Hashing password
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-
-  try {
-    const hashedPassword = await bcrypt.hash(this.password, 10);
-    this.password = hashedPassword;
-    next();
-  } catch (error) {
-    return next(error);
-  }
-});
-
-userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
-
-userSchema.methods.generateAccessToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-      email: this.email,
-      firstName: this.firstName,
-      lastName: this.lastName,
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-    }
-  );
-};
-userSchema.methods.generateRefreshToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-    }
-  );
-};
-
-const user = mongoose.model("user", userSchema);
-module.exports = user;
+module.exports = mongoose.model('users', userSchema);
