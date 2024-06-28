1c9c96206496f7508a129f3f0a221a06f11b81b6
amj <169126481+amjoftllb@users.noreply.github.com>
2024-06-28 09:46:45 +0530

    Delete project 1 Typscript state/redux directory

--- project 1 Typscript state/redux/reducer/rootReducer.tsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { Chats } from "./chat";
-const rootReducer = combineReducers({
-  Chats: Chats,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
-
-export default rootReducer;
