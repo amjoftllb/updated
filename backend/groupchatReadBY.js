e2234b576318ed7c4171680b605773e1436efae4
amj <169126481+amjoftllb@users.noreply.github.com>
2024-07-24 12:39:05 +0530

    Create groupchatReadBY.js

---
+++ backend/groupchatReadBY.js
@@ -0,0 +1,53 @@
+const { Server } = require("socket.io");
+const chatRoom = require("../models/chatRoom.js");
+
+function initializeChatSocket(server) {
+  const io = new Server(server);
+
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+  
+    socket.on('joinRoom', async (roomId) => {
+      socket.join(roomId);
+      console.log(`User joined room ${roomId}`);
+    });
+  
+    socket.on('sendMessage', async (data) => {
+      const { roomId, text, sender } = data;
+      const message = {
+        text,
+        sender,
+        readBy: [] // Initialize without adding the sender
+      };
+  
+      const updatedRoom = await chatRoom.findOneAndUpdate(
+        { roomId },
+        { $push: { messages: message } },
+        { new: true, useFindAndModify: false }
+      );
+  
+      io.to(roomId).emit('messageReceived', updatedRoom);
+    });
+  
+    socket.on('markMessageAsRead', async (data) => {
+      const { roomId, messageId, userId } = data;
+  
+      await chatRoom.updateOne(
+        { roomId, 'messages._id': messageId },
+        { $addToSet: { 'messages.$.readBy': { userId, isRead: true, readAt: new Date() } } },
+        { new: true }
+      );
+  
+      const updatedRoom = await chatRoom.findOne({ roomId });
+      io.to(roomId).emit('messageRead', updatedRoom);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+  return io;
+}
+
+module.exports = initializeChatSocket;
