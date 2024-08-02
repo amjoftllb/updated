a10b71c2ddc16e44566013d4ed1cd976c0040f62
amj <169126481+amjoftllb@users.noreply.github.com>
2024-08-02 10:48:11 +0530

    Update MainHomePage.tsx

--- nextjs/MainHomePage.tsx
+++ nextjs/MainHomePage.tsx
@@ -1,11 +1,25 @@
+// import React from 'react';
+
+// const HomePage: React.FC = () => {
+//   return (
+//     <div>
+//       <h1>Welcome to the Home Page</h1>
+//       <p>This is the main page of your application.</p>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
+
 import React from 'react';
+import MainLayout from './layout';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
+    <MainLayout>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main page of your application.</p>
-    </div>
+    </MainLayout>
   );
 };
 
