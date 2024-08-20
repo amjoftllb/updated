751237b5b2ce8633191ac55299744c01569b1167
amj <169126481+amjoftllb@users.noreply.github.com>
2024-08-20 14:44:37 +0530

    Create react.Modal.jsx

---
+++ frontend/react.Modal.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+function App() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        {/* Modal content */}
+      </Modal>
+    </div>
+  );
+}
+
+export default App;
