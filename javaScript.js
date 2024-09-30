e8c22fc2d7f69cbfda6f9aa08c0e32c78e8b7f70
amj <169126481+amjoftllb@users.noreply.github.com>
2024-09-30 17:09:19 +0530

    Create javaScript.js

---
+++ javaScript.js
@@ -0,0 +1,66 @@
+const a=["amj" , "jba" , "hle" , "bsh" ,"mde"]
+const b=["amj" , "jba" , "hle" , "bsh" ,"mde"]
+const c=["3" , "5" , "9" , "7" ,"6"]
+const d=["35" , "25" , "95" , "77" ,"16"]
+console.log(a);
+
+//-------------------------------Basic Methods-----------------------------
+
+// const answer = a.length
+
+// const answer = a.toString()
+
+// const answer = a.at(2)
+
+//const answer = a.join(".")
+
+// const answer = a.pop()
+
+// const answer = a.shift()
+
+// const answer = a.unshift()
+
+// const answer = a.concat(b)
+
+// a.splice(2,0, "Lemon", "Kiwi");
+
+// const answer = a.slice(1,3);
+// const answer = a.slice(-3);
+
+// const answer = [[1,2],[3,4],[5,6]];
+// const newArr = answer.flat();
+// console.log(newArr);
+
+//-------------------------------search Methods-----------------------------
+
+//const answer = a.indexOf("hle")
+//const answer = a.includes("hle")
+
+//const answer = c.find(value => value > 4)
+//const answer = c.findIndex(value => value > 7)
+
+
+//-------------------------------sort Methods-----------------------------
+
+//const answer = a.sort()
+//const answer = c.sort()  // produce incorrect result when sorting numbers.
+
+//There are no built-in functions for finding the max or min value in an array.
+
+//const answer = a.toSorted()
+//const answer = c.toSorted()
+
+//const answer = a.reverse()
+//const answer = c.reverse()
+
+//const answer = a.toReversed()
+//const answer = c.toReversed()
+
+//-------------------------------iteration Methods-----------------------------
+
+
+
+//-----------------------------------------------------------------------------
+
+console.log(answer);
+console.log(a);
